cae20d7353099b0330c0b405c36d872b8749a49e
weqopy <weqopy@gmail.com>
2018-06-03 18:35:08 +0800

    update 404

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, make_response
 
 app = Flask(__name__)
 
@@ -26,6 +26,11 @@ def get_task(task_id):
     if len(task) == 0:
         abort(404)
     return jsonify({'tasks': task[0]})
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
