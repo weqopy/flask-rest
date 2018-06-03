cb9b71bc894f95ed23da6410df8320ec7923500f
weqopy <weqopy@gmail.com>
2018-06-03 18:42:04 +0800

    forth api, update_task

--- app.py
+++ app.py
@@ -47,5 +47,26 @@ def create_task():
     return jsonify({'task': task}), 201
 
 
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = list(filter(lambda t: t['id'] == task_id, tasks))
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(
+            request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description',
+                                              task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+
 if __name__ == '__main__':
     app.run(debug=True)
