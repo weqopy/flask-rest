c0d570e24df0810bfa8da74be81c860a44453f19
weqopy <weqopy@gmail.com>
2018-06-03 18:27:51 +0800

    first api, get_tasks

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+tasks = [{
+    'id': 1,
+    'title': 'Buy groceries',
+    'description': 'Milk, Cheese, Pizza',
+    'done': False
+}, {
+    'id': 2,
+    'title': 'Learn Python',
+    'description': 'Need to find a good Python tutorial on the web',
+    'done': False
+}]
 
-@app.route('/')
-def index():
-    return "Hello World"
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
 
 
 if __name__ == '__main__':
